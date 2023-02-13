f2a445b6d0fcb0491fa76b5e4ac7bca6b3112daa
hosmi <koike.engineer@gmail.com>
2023-02-13 22:35:39 +0900

    change access point

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -10,7 +10,7 @@ const CreatePost = () => {
   const [form, setForm] = useState({
     name: '',
     prompt: '',
-    photo: ''
+    photo: '',
   })
   const [generatingImg, setgeneratingImg] = useState(false)
   const [loading, setLoading] = useState(false)
@@ -22,13 +22,16 @@ const CreatePost = () => {
         // img読み込みの設定
         setgeneratingImg(true)
         // レスポンスの取得
-        const response = await fetch('http://localhost:8080/api/v1/dalle', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({ prompt: form.prompt })
-        })
+        const response = await fetch(
+          'https://dali-e.onrender.com/api/v1/dalle',
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ prompt: form.prompt }),
+          }
+        )
 
         const data = await response.json()
 
@@ -57,13 +60,16 @@ const CreatePost = () => {
 
       // 投稿するエンドポイントと通信を行う。
       try {
-        const response = await fetch('http://localhost:8080/api/v1/post', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(form)
-        })
+        const response = await fetch(
+          'https://dali-e.onrender.com/api/v1/post',
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(form),
+          }
+        )
         await response.json()
         navigate('/')
       } catch (err) {
@@ -91,75 +97,78 @@ const CreatePost = () => {
   }
 
   return (
-    <section className="max-w-7xl mx-auto">
-      <div className="">
-        <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
-        <p className="mt-2 text-[#666e75] text-[14px] max-w-[500px]">
+    <section className='max-w-7xl mx-auto'>
+      <div className=''>
+        <h1 className='font-extrabold text-[#222328] text-[32px]'>Create</h1>
+        <p className='mt-2 text-[#666e75] text-[14px] max-w-[500px]'>
           Generate an imaginative image through DALL-E AI and share it with the
           community
         </p>
       </div>
 
-      <form onSubmit={handleSubmit} className="mt-16 max-w-3xl">
-        <div className="flex flex-col gap-5">
+      <form
+        onSubmit={handleSubmit}
+        className='mt-16 max-w-3xl'
+      >
+        <div className='flex flex-col gap-5'>
           <FormField
-            labelName="Your Name"
-            type="text"
-            name="name"
-            placeholder="Ex., John Doe"
+            labelName='Your Name'
+            type='text'
+            name='name'
+            placeholder='Ex., John Doe'
             value={form.name}
             handleChange={handleChange}
           />
           <FormField
-            labelName="Prompt"
-            type="text"
-            name="prompt"
-            placeholder="An Impressionist oil painting of sunflowers in a purple vase…"
+            labelName='Prompt'
+            type='text'
+            name='prompt'
+            placeholder='An Impressionist oil painting of sunflowers in a purple vase…'
             value={form.prompt}
             handleChange={handleChange}
             isSurpriseMe
             handleSurpriseMe={handleSurpriseMe}
           />
           {/* 生成された画像を表示するための場所 */}
-          <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 h-64 p-3 flex justify-center items-center">
+          <div className='relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 h-64 p-3 flex justify-center items-center'>
             {form.photo ? (
               <img
                 src={form.photo}
                 alt={form.prompt}
-                className="w-full h-full object-contain"
+                className='w-full h-full object-contain'
               />
             ) : (
               <img
                 src={preview}
-                alt="preview"
-                className="w-9/12 h-9/12 object-contain opacity-40"
+                alt='preview'
+                className='w-9/12 h-9/12 object-contain opacity-40'
               />
             )}
             {generatingImg && (
-              <div className="absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg">
+              <div className='absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg'>
                 <Loader />
               </div>
             )}
           </div>
         </div>
 
-        <div className="mt-5 flex gap-5">
+        <div className='mt-5 flex gap-5'>
           <button
-            type="button"
+            type='button'
             onClick={generateImage}
-            className="text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+            className='text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center'
           >
             {generatingImg ? 'Generating...' : 'Generate'}
           </button>
         </div>
-        <div className="mt-10">
-          <p className="mt-2 text-[#666e75] text-[14px]">
+        <div className='mt-10'>
+          <p className='mt-2 text-[#666e75] text-[14px]'>
             **Once you have created the image you want, you can share it with
             others in the community**
           </p>
           <button
-            type="submit"
-            className="mt-3 text-white text-sm bg-[#6469ff] font-medium rounded-md w-full sm:w-auto px-5 py-2.5 text-center"
+            type='submit'
+            className='mt-3 text-white text-sm bg-[#6469ff] font-medium rounded-md w-full sm:w-auto px-5 py-2.5 text-center'
           >
             {loading ? 'Sharing...' : 'Share with the Community'}
           </button>
